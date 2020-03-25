4f938c45e34a4ca6bca2f7145391ede079a193a8
ZiadTarik <62615583+ziadtarik@users.noreply.github.com>
2020-03-25 16:07:14 +0300

    Update YasaShken.py

--- YasaShken.py
+++ YasaShken.py
@@ -12,8 +12,8 @@ import time,sys,socket,threading,logging,urllib.request,random
 
 def user_agent():
 	global uagent	
-  uagent=[]
-  uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+        uagent=[]
+        uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
 	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
 	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
